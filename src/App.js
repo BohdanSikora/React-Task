2d27a0e817b3e3c4685461f56acdaf1b4e84bb8f
Bohdan Sikora <bohdansikora@Bohdans-MacBook-Air.local>
2022-02-03 00:51:36 +0200

    Task 1 react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
+import MainPage from './components/MainPage/MainPage';
+import Hero from './components/Hero/Hero';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
   return ( 
     <div className ="App">
       <Navbar/>
+      <Hero/>
+      <MainPage/>
+      <Footer/>
     </div>
-
+    
   );
-  
 }
 
 export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+
+import s from './Footer.module.css';
+import SvgSelector from '../icons/SvgSelector';
+export default function Footer() {
+  return (
+    <div className={s.footer__section}>
+      <div className={s.footer__mainInfo}>
+        <div className={s.footer__stuff}>
+          <h3 className={s.footer__title}>Branding stuff</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+            totam?
+          </p>
+        </div>
+        <h1 className={s.footer__logo}>Shoes</h1>
+        <ul className={s.footer__socials}>
+          <li className={s.footer__icon}>
+            <SvgSelector id="facebook" />
+          </li>
+          <li className={s.footer__icon}>
+            <SvgSelector id="twitter" />
+          </li>
+          <li className={s.footer__icon}>
+            <SvgSelector id="linkedin" />
+          </li>
+          <li className={s.footer__icon}>
+            <SvgSelector id="google" />
+          </li>
+        </ul>
+      </div>
+      <p className={s.footer_copyright}>2022 @ Copyright all rigts reserved</p>
+    </div>
+  );
+}
